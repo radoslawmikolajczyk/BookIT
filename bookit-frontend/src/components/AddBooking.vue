5d67926ec4320f6997ec4c59650d5c7587ff0bf5
xilodro <jaroslawj@student.agh.edu.pl>
2024-04-10 01:56:06 +0200

    configuration

---
+++ bookit-frontend/src/components/AddBooking.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import { onBeforeMount, ref } from 'vue'
+import { GroupsService } from './services/groupsService'
+
+    const dogs = ref("")
+    const services = new GroupsService()
+
+
+    onBeforeMount( async() => {
+        dogs.value = services.getAllGroups().then( result => dogs.value = result)
+    })
+
+    function insertData(){
+        services.createGroups().then( result => dogs.value = result)
+    }
+
+</script>
+
+<template>
+    <button @click="insertData">Click Me!</button>
+</template>
+
+<style scoped>
+</style>
